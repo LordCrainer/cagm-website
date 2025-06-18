6397c9ce56a77c098697f7746d84d2f18640535b
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-06-18 18:41:21 -0500

    feat: add initial configuration files for Nuxt 3, including Prettier, Tailwind CSS, and NVM settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
 
+  css: ['~/assets/css/main.css'],
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/test-utils',
+    '@nuxtjs/tailwindcss',
     '@nuxt/ui'
   ]
 })
